602f8bb9533715aa51ed061ccffb581e9d459e05
Dave Uncle <Dave Uncle>
2017-03-20 15:39:19 +0100

    filter array

--- src/app/shared/book-data.service.ts
+++ src/app/shared/book-data.service.ts
@@ -14,21 +14,26 @@ export class BookDataService {
     .map(response => response.json())
   }
 
-//  books = [
-//   {
-//     "title": "Design Patterns",
-//     "isbn": "978-0-20163-361-0",
-//     "author": "Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides"
-//   },
-//   {
-//     "title": "REST und HTTP",
-//     "isbn": "978-3-86490-120-1",
-//     "author": "Stefan Tilkov / Martin Eigenbrodt / Silvia Schreier / Oliver Wolf"
-//   },
-//   {
-//     "title": "Eloquent JavaScript",
-//     "isbn": " 978-1-59327-584-6",
-//     "author": "Marijn Haverbeke"
-//   }
-// ];
+ books = [
+  {
+    "title": "Design Patterns",
+    "isbn": "978-0-20163-361-0",
+    "author": "Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides"
+  },
+  {
+    "title": "REST und HTTP",
+    "isbn": "978-3-86490-120-1",
+    "author": "Stefan Tilkov / Martin Eigenbrodt / Silvia Schreier / Oliver Wolf"
+  },
+  {
+    "title": "Eloquent JavaScript",
+    "isbn": " 978-1-59327-584-6",
+    "author": "Marijn Haverbeke"
+  }
+];
+getOneBook(isbn){
+  return this.books.filter((b) =>{
+    return b.isbn == isbn;
+  })
+}
 }
